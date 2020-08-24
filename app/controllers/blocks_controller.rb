844cdb6cf9481695bec5ed536203999c5f50e0b7
Josh Ellis <josh@imjoshellis.com>
2020-08-24 13:44:06 -0500

    Remove show method

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -4,11 +4,6 @@ class BlocksController < ApplicationController
     render json: BlockSerializer.new(blocks).to_serialized_json
   end
 
-  def show
-    block = Block.find_by(id: params[:id])
-    render json: BlockSerializer.new(block).to_serialized_json
-  end
-
   def update
     block = Block.find_by(id: params[:id])
     block.update(task_list: params[:task_list])
