cfb1a0210ea32116d25d164e0c51e171d896afac
Josh Ellis <josh@imjoshellis.com>
2020-08-24 16:57:46 -0500

    Add spacing in controller methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,12 +15,14 @@ class TasksController < ApplicationController
     block = Block.find_by(block_id: params[:block_id])
     task = Task.create(title: params[:title], completed: false, dueDate: params[:dueDate], block_id: params[:block_id])
     block.task_list = block.task_list + "," + task.id
+
     render json: TaskSerializer.new(task).to_serialized_json
   end
 
   def update
     task = Task.find(params[:id])
     task.update(title: params[:title], completed: params[:completed], dueDate: params[:dueDate], block_id: params[:block_id])
+
     render json: TaskSerializer.new(task).to_serialized_json
   end
 
@@ -28,6 +30,7 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task_json = TaskSerializer.new(task)
     task.destroy
+
     render json: task_json.to_serialized_json
   end
 end
