63de8424faa3523be2025698fd5a0696382257f4
Josh Ellis <josh@imjoshellis.com>
2020-08-24 14:27:31 -0500

    Add block validations

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,8 @@
 class Block < ApplicationRecord
+  enum kind: [:life, :year, :quarter, :month, :week, :day]
   has_many :tasks, dependent: :destroy
+  validates :title, presence: true
+  validates :kind, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
