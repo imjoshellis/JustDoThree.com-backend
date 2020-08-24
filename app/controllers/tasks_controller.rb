5f8241d6222c4d4b9184dd5ccfa82a9338d5d406
Josh Ellis <josh@imjoshellis.com>
2020-08-24 16:48:31 -0500

    Add self.generate_year method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,28 @@
 class TasksController < ApplicationController
+  def index
+    tasks = []
+    blocks = Block.current_blocks
+    blocks.each do |b|
+      b.tasks.each do |t|
+        tasks << t
+      end
+    end
+
+    render json: TaskSerializer.new(tasks).to_serialized_json
+  end
+
   def create
     block = Block.find_by(block_id: params[:block_id])
     task = Task.create(title: params[:title], completed: false, dueDate: params[:dueDate], block_id: params[:block_id])
     block.task_list = block.task_list + "," + task.id
+
     render json: TaskSerializer.new(task).to_serialized_json
   end
 
   def update
     task = Task.find(params[:id])
     task.update(title: params[:title], completed: params[:completed], dueDate: params[:dueDate], block_id: params[:block_id])
+
     render json: TaskSerializer.new(task).to_serialized_json
   end
 
@@ -16,6 +30,7 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task_json = TaskSerializer.new(task)
     task.destroy
+
     render json: task_json.to_serialized_json
   end
 end
--- app/models/block.rb
+++ app/models/block.rb
@@ -5,4 +5,20 @@ class Block < ApplicationRecord
   validates :kind, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  def self.generate(year)
+    title = "Y" + year.to_s
+    b = Block.new(title: title, start_date: Date.new(year, 1, 1), end_date: Date.new(year, 12, 31))
+    b.year!
+    b.make_quarters
+    life_block = Block.find_by(kind: 0)
+    life_list = life_block.block_list
+    if life_list
+      life_list += "," + b.id
+    else
+      life_list = b.id
+    end
+    life_block.update(block_list: life_list)
+    b
+  end
+
 end
