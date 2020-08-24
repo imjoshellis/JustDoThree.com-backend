fd4b9d45e403c5eace452992eec6f5ecc05a93d8
Josh Ellis <josh@imjoshellis.com>
2020-08-24 14:28:05 -0500

    Add default to completed

--- db/migrate/20200824135703_create_tasks.rb
+++ db/migrate/20200824135703_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks, id: :uuid do |t|
       t.string :title
-      t.boolean :completed
+      t.boolean :completed, default: false
       t.string :dueDate
       t.string :block_id
 
