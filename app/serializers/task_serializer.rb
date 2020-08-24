bec6d11284feb2dceb3b1a07da3c80e81886b719
Josh Ellis <josh@imjoshellis.com>
2020-08-24 14:27:56 -0500

    Setup task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,12 @@
 class TaskSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes
+  def initialize(o)
+    @task = o
+  end
+
+  def to_serialized_json
+    options = {
+      only: [:id, :title, :dueDate, :block_id]
+    }
+    @task.to_json(options)
+  end
 end
