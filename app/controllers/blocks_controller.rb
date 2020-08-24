610dd306628d47408cdad7a604a16f315e2125bf
Josh Ellis <josh@imjoshellis.com>
2020-08-24 16:51:08 -0500

    Revert "Add self.generate_year method"
    5f8241d6222c4d4b9184dd5ccfa82a9338d5d406

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,14 +1,12 @@
 class BlocksController < ApplicationController
   def index
-    blocks = Block.current_blocks
-    
+    blocks = Block.all
     render json: BlockSerializer.new(blocks).to_serialized_json
   end
 
   def update
-    block = Block.find(params[:id])
+    block = Block.find_by(id: params[:id])
     block.update(task_list: params[:task_list])
-
     render json: BlockSerializer.new(block).to_serialized_json
   end
 end
