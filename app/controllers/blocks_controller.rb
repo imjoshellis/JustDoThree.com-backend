0ae76113d8f5f7c997d486921f4fcc6bc0d3847e
Josh Ellis <josh@imjoshellis.com>
2020-08-24 16:57:25 -0500

    Add spacing in controller methods

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,12 +1,14 @@
 class BlocksController < ApplicationController
   def index
     blocks = Block.current_blocks
+
     render json: BlockSerializer.new(blocks).to_serialized_json
   end
 
   def update
     block = Block.find_by(id: params[:id])
     block.update(task_list: params[:task_list])
+
     render json: BlockSerializer.new(block).to_serialized_json
   end
 end
