11a47660cb6eab564101e83f3c736e9b988ca06b
Josh Ellis <josh@imjoshellis.com>
2020-08-24 17:23:58 -0500

    Add block_title method

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :block
   validates :title, presence: true
   validates :block_id, presence: true
+
+  def block_title
+    Block.find(block_id).title
+  end
 end
