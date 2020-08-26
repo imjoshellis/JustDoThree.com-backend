a296c8883ceda7245a1b792c7460e8afbf9981e4
Josh Ellis <josh@imjoshellis.com>
2020-08-26 16:33:36 -0500

    Send start/end dates via API

--- app/serializers/block_serializer.rb
+++ app/serializers/block_serializer.rb
@@ -1,5 +1,5 @@
 class BlockSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :title, :kind, :block_list, :task_list
+  attributes :title, :kind, :block_list, :task_list, :start_date, :end_date
 end
